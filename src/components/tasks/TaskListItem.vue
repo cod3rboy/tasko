fd8985afdb3882f4e8c9d4e2e37e8431dc41a2cb
Dheeraj Kumar <dkcalotra@hotmail.com>
2021-02-17 11:47:09 +0530

    TaskListItem component is now clickable

--- src/components/tasks/TaskListItem.vue
+++ src/components/tasks/TaskListItem.vue
@@ -5,8 +5,9 @@
         <base-checkbox
           :checked="taskItem.finished"
           @check-change="setTaskStatus"
+          @click.stop
         ></base-checkbox>
-        <div class="detail">
+        <div class="detail" tabindex="0">
           <div class="title">{{ taskItem.title }}</div>
           <div class="date">Due Date : {{ dueDate }}</div>
         </div>
@@ -34,10 +35,25 @@ export default {
 </script>
 <style scoped>
 .flex-container {
+  position: relative;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
   gap: 0.2em;
+  padding: 0.5em;
+  cursor: pointer;
+}
+.flex-container:active::after {
+  /* Ripple pseudo element */
+  display: block;
+  content: " ";
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.05);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  pointer-events: none;
 }
 .flex-container > .detail {
   flex: 1 1 auto;
