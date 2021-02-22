b1e409d136fd80bfb7746850f7a4a07e61674e1d
Dheeraj Kumar <dkcalotra@hotmail.com>
2021-02-22 10:59:25 +0530

    Added CategoryListItem component.

---
+++ src/components/category/CategoryListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-card>
+    <base-selectable-list-item>
+      {{ category.name }}
+    </base-selectable-list-item>
+  </base-card>
+</template>
+
+<script>
+export default {
+  props: {
+    category: {
+      type: Object,
+      required: true,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/pages/ManageCategories.vue
+++ src/pages/ManageCategories.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="card-container">
-    <ul>
-      <li v-for="category in categories" :key="category.id">
-        {{ category.name }}
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li class="category-item" v-for="category in categories" :key="category.id">
+      <category-list-item :category="category"></category-list-item>
+    </li>
+  </ul>
 </template>
 
 <script>
+import CategoryListItem from "../components/category/CategoryListItem.vue";
+
 export default {
+  components: {
+    CategoryListItem,
+  },
+  data() {
+    return {
+      selectedItems: [],
+    };
+  },
   computed: {
     categories() {
       return this.$store.getters["category/categories"];
@@ -19,5 +27,20 @@ export default {
 </script>
 
 <style scoped>
-
+ul {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: 1fr;
+  grid-gap: 0.6em;
+  list-style: none;
+  font-size: 1.5rem;
+  padding: 0;
+  margin: 0;
+  max-height: 100%;
+  overflow: hidden auto;
+}
+.category-item {
+  position: relative;
+  margin: 0 0.5em;
+}
 </style>
