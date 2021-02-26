09a25b9f808360f3574fa4a3d0b9da5f10d7df2a
Dheeraj Kumar <dkcalotra@hotmail.com>
2021-02-26 12:45:12 +0530

    Login page designed.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,50 @@
 <template>
   <base-page title="Start using TASKO" subtitle="Login with your credentials">
-    <form class="login-form">
-        <div class="form-control">
-            <label for=""></label>
-        </div>
+    <form @submit.prevent="login">
+      <base-form-control
+        label="Email Address"
+        control-id="useremail"
+        :has-error="false"
+        invalid-message="Invalid Email"
+      >
+        <input
+          type="email"
+          id="useremail"
+          v-model.trim="userEmail"
+          autocomplete="false"
+        />
+      </base-form-control>
+
+      <base-form-control
+        label="Password"
+        control-id="userpassword"
+        :has-error="false"
+        invalid-message="Invalid Password"
+      >
+        <input
+          type="password"
+          id="userpassword"
+          v-model.trim="userPassword"
+          autocomplete="false"
+        />
+      </base-form-control>
+      <base-button look="accent">Log in</base-button>
     </form>
   </base-page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userEmail: "",
+      userPassword: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("Logged IN");
+    },
+  },
+};
+</script>
