d8fc9f2f329ab9bf51b619ca8381cd0dbf7de449
Dheeraj Kumar <dkcalotra@hotmail.com>
2021-02-27 11:43:13 +0530

    Added validation in signup page.

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,30 +1,63 @@
 <template>
   <base-page title="Signup" subtitle="Start using TASKO">
-    <base-form @submit.prevent>
-      <base-form-control label="Full Name" control-id="fullName">
+    <base-form @submit.prevent="signup">
+      <base-form-control
+        label="Full Name"
+        control-id="fullName"
+        :has-error="!fullNameValidation.isValid"
+        :invalid-message="fullNameValidation.invalidMessage"
+      >
         <input
           type="text"
           id="fullName"
           autocomplete="false"
           v-model.trim="userFullName"
+          @blur="tryValidateFullName"
         />
       </base-form-control>
 
-      <base-form-control label="Email Address" control-id="useremail">
+      <base-form-control
+        label="Email Address"
+        control-id="useremail"
+        :has-error="!emailValidation.isValid"
+        :invalid-message="emailValidation.invalidMessage"
+      >
         <input
           type="email"
           id="useremail"
           v-model.trim="userEmail"
           autocomplete="false"
+          @blur="tryValidateUserEmail"
         />
       </base-form-control>
 
-      <base-form-control label="Password" control-id="userpassword">
+      <base-form-control
+        label="Password"
+        control-id="userpassword"
+        :has-error="!pwdValidation.isValid"
+        :invalid-message="pwdValidation.invalidMessage"
+      >
         <input
           type="password"
           id="userpassword"
           v-model.trim="userPassword"
           autocomplete="false"
+          @blur="tryValidatePassword"
+        />
+      </base-form-control>
+
+      <base-form-control
+        label="Confirm Password"
+        control-id="confirmpassword"
+        :has-error="!confirmPwdValidation.isValid"
+        :invalid-message="confirmPwdValidation.invalidMessage"
+      >
+        <input
+          type="password"
+          id="confirmpassword"
+          v-model.trim="confirmPassword"
+          autocomplete="false"
+          @blur="tryValidateConfirmPassword"
         />
       </base-form-control>
 
@@ -44,12 +77,62 @@
 
 <script>
 export default {
+  // TODO : Use email regex for validation
   data() {
     return {
       userFullName: "",
       userEmail: "",
       userPassword: "",
+      confirmPassword: "",
+      fullNameValidation: {
+        isValid: true,
+        invalidMessage: "Your name cannot be empty",
+      },
+      emailValidation: {
+        isValid: true,
+        invalidMessage: "Please enter a valid email address",
+      },
+      pwdValidation: {
+        isValid: true,
+        invalidMessage: "Password must be atleast six characters long",
+      },
+      confirmPwdValidation: {
+        isValid: true,
+        invalidMessage: "Passwords do not match",
+      },
     };
+  },
+  methods: {
+    tryValidateFullName() {
+      this.fullNameValidation.isValid = this.userFullName.length > 0;
+    },
+    tryValidateUserEmail() {
+      this.emailValidation.isValid = this.userEmail.indexOf("@") > 0;
+    },
+    tryValidatePassword() {
+      this.pwdValidation.isValid = this.userPassword.length >= 6;
+    },
+    tryValidateConfirmPassword() {
+      this.confirmPwdValidation.isValid =
+        this.confirmPassword === this.userPassword;
+    },
+    validateData() {
+      this.tryValidateFullName();
+      this.tryValidateUserEmail();
+      this.tryValidatePassword();
+      this.tryValidateConfirmPassword();
+      return (
+        this.fullNameValidation.isValid &&
+        this.emailValidation.isValid &&
+        this.pwdValidation.isValid &&
+        this.confirmPwdValidation.isValid
+      );
+    },
+    signup() {
+      if (!this.validateData()) return;
+      // Sign up the user
+      console.log("Signed Up");
+    },
   },
 };
 </script>
